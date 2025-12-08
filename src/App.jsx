aca68245fb72693c4341e6a348a2e9c2be6fad90
Tawhid Shakil <astalukdar39@gmail.com>
2025-12-08 12:14:06 +0600

    This is our first commit for this project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to our Kingom. Currently we are under the construction. We will back soon, Thanks for patience!</h1>
+    </>
+  )
+}
+
+export default App
